ab7f830a12cd728c14a5783e53fc821f0a579d6d
PraveenGirishNadumani <praveen.gn@razorpay.com>
2021-02-25 21:19:09 +0530

    Adding scheduler for the message queue

--- controller/PostService.js
+++ controller/PostService.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
 const { response } = require('express');
+const cron = require('node-cron');
 const express = require('express');
+const env = require('dotenv').config();
 const mongoose = require('mongoose');
 const Post = require('../models/postModel');
 
@@ -9,7 +11,7 @@ const app = express.Router();
 app.use(bodyParser.json());
 
 //Connecting to MongoBD server.
-mongoose.connect('mongodb+srv://praveen:praveen@cluster0.tbwf2.mongodb.net/Posts', {useNewUrlParser: true, useUnifiedTopology: true  });
+mongoose.connect(process.env.MongoUrl, {useNewUrlParser: true, useUnifiedTopology: true  });
 
 
 //Post message to the MongoBD
@@ -19,11 +21,33 @@ app.post('/', (req,resp) => {
     post.message = req.body.message;
     post.date = req.body.date;
     post.time = req.body.time;
-    post.save()
-    .then((response) => {
-        resp.status(200).send("Done"+response);
-    })
-    .catch(error => resp.status(400).send("error"+ error))
+
+    // Getting the epoc values from the humar readable time
+    var postTimeValue = req.body.date+ " : "+ req.body.time+ " : " +req.body.GMT;
+    var date = new Date(postTimeValue);
+    console.log(date.getTime()+" time value: "+ postTimeValue);
+    var endTime = Date.now();
+    
+    //Convering the time difference into miliseconds, so that we could add this to the event queue for scheduling the message.
+    
+    var waitTime = (endTime-date.getTime())/1000;
+
+    //Creating a scheduler with setTimeoutFunction based on the request date and time.
+    setTimeout(() => {
+        post.save()
+        .then((response) => {
+            console.log('message updated to the mongo');
+        })
+        .catch(error => resp.status(400).send("error"+ error))
+    }, waitTime);
+
+    console.log(waitTime);
+    console.log(date.getTime());
+    console.log(endTime);
+
+    resp.status(200).send("Done, message scheduled");
+
+
 });
 
 //fetch all the message from the mongoDB
